8506e5c9ce3c37895e284af132ae8f87a4fde1bd
hiro-nishino <126562444+hiro-nishino@users.noreply.github.com>
2023-03-14 22:04:09 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -40,8 +40,9 @@ class PrototypesController < ApplicationController
 
   def destroy
     prototype = Prototype.find(params[:id])
-    prototype.destroy
-    redirect_to root_path
+    if prototype.destroy
+       redirect_to root_path
+    end
   end
 
   private
